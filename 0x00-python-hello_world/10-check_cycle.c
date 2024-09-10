c38d0514a009ca7509178cfd44621cfe41f49ff5
Hajar Hafdi <hajarlakbir@outlook.com>
2024-09-09 20:23:23 -0700

    fixing errors

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -14,7 +14,7 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list;
 
-	while (slow && fast->next)
+	while (fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
