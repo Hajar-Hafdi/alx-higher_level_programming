a47361376311b2856990ad4b4a645a24d5b818f0
Hajar Hafdi <hajarlakbir@outlook.com>
2024-09-10 13:26:04 -0700

    fixing errors

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for u in range(10):
     for k in range(u, 10):
         if u < k:
-            print("{:d} {:d}".format(u, k),
+            print("{:d}{:d}".format(u, k),
                     end="\n" if u == 8 and k == 9 else ", ")
