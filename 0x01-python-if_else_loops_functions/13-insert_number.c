8a77437047c068a5842854ba609eb968e048e42e
Hajar Hafdi <hajarlakbir@outlook.com>
2024-09-10 14:24:23 -0700

    added 13-insert_number.c

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/*
+ * insert_node - inserts nd in srtd lst
+ *
+ * @head: add of hd ptr
+ * @number: num to be inserted
+ *
+ * Return: returns inserted nd
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *nd = *head, *nw = malloc(sizeof(listint_t));
+
+	if (!nw)
+		return (NULL);
+	nw->n number;
+	nw->next = NULL;
+
+	if (!nd || nw->n < nd->n)
+	{
+		nw->next = nd;
+		return (*head = nw);
+	}
+	while (nd)
+	{
+		if (!nd->next || new->n < nd->next->n)
+		{
+			nw->next = nd->next;
+			nd->next = nw;
+			return (nd);
+		}
+		nd = nd-.next;
+	}
+	return (NULL);
+}
