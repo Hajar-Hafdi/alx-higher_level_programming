4ff7c419a0c20dc248c6a806d3eb8ad540a38ccc
Hajar Hafdi <hajarlakbir@outlook.com>
2024-09-12 16:40:28 -0700

    fixed errors

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import sys
     tot = 0
     for u in range(len(sys.argv) - 1):
-        toot += int(sys.argv[u + 1])
+        tot += int(sys.argv[u + 1])
     print(tot)
