4d982759b7444c031796c442a4554fd21ec61917
Hajar Hafdi <hajarlakbir@outlook.com>
2024-09-12 17:22:12 -0700

    fixing errors

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import variable_load_5
-    print(variable_load_5.a)
+    """Show the value of the var a from variable_load_5."""
+    import a from variable_load_5
+    print(a)
