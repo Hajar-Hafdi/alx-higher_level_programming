46a58b8273c57ca8d0a36bfd97c94454737e4007
Hajar Hafdi <hajarlakbir@outlook.com>
2024-09-16 16:02:37 -0700

    fixing errors in 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    lstdvs = []
-    for u in my_list:
-        if (u % 2) == 0:
-            lstdvs.append(True)
-        else:
-            lstdvs.append(False)
-     return lstdvs
+    if len(my_list) == 0:
+        return None
+    maxi_value = my_list[0]
+    for ber in my_list:
+        if ber > maxi_value:
+            maxi_value = ber
+    return maxi_value
