423ec64a6bd540dce68c8a40023a46c86908c331
Hajar Hafdi <hajarlakbir@outlook.com>
2024-09-16 15:48:44 -0700

    fixing errors

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return None
     for smatrix in matrix:
         for u in range(len(smatrix)):
-                print("{:d}".format(smatrix[u]),
-                        end="\n" if u == len(smatrix) - 1 else " ")
+            print("{:d}".format(smatrix[u]),
+                        end=" " if u != len(smatrix) - 1 else "")
         print()
