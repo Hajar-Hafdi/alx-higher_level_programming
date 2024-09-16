a2d05a3c8bb52aa5bd12cc15e06ed568ab61f7ba
Hajar Hafdi <hajarlakbir@outlook.com>
2024-09-16 16:36:10 -0700

    fixing errors in 13

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -23,7 +23,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-
+int x_pal(listint_t **head, listint_t *ennd);
 
 def print_list_integer(my_list=[]):
 def element_at(my_list, idx):
