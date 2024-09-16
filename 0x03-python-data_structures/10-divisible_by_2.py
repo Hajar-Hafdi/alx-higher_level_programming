9c2b2ed7b7640f44d65b65ba392e053a6a93872f
Hajar Hafdi <hajarlakbir@outlook.com>
2024-09-16 15:56:10 -0700

    fixing errors in 10 divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my-list is None:
+    if my_list is None:
         return None
     lstdvs = []
     for u in my_list:
         if (u % 2) == 0:
-            lstdvs.appen(True)
+            lstdvs.append(True)
         else:
             lstdvs.append(False)
      return lstdvs
