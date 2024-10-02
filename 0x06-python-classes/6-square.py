98720487069396d62631a4c376b24c1f7f4cf638
Hajar Hafdi <hajarlakbir@outlook.com>
2024-10-01 22:01:23 -0700

    fixing errors

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,8 +47,8 @@ class Square:
             print("")
             return
 
-    [print("") for ur in range(0, self.__position[1])]
-    for ur in range(0, self.__size):
-        [print(" ", end="") for k in range(0, self.__position[0])]
-        [print("#", end="") for l in range(0, self.__size)]
+    for _ in range(self.__position[1]):
         print("")
+
+    for _ in range(self.__size):
+        print(" " * self.__position[0] + "#" * self.__size)
