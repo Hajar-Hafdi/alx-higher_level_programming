85815c729077d3da4f2064294ba04d270a78f410
Hajar Hafdi <hajarlakbir@outlook.com>
2024-10-02 17:33:21 -0700

    fixed errors in 5 file

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -29,9 +29,10 @@ class Square:
             value: size of square
         """
         if not isinstance(value, int):
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """calcultae and return the area of sqaure
