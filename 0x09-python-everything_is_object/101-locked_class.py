73abf8edcc7a50373e32ac0ba9182417f5b0abd6
Hajar-Hafdi <Hajarlakbir@outlook.com>
2024-10-08 12:48:08 -0700

    adding files

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""Define locked class"""
+
+class LockedClass:
+    """
+    prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name
+    """
+
+    __slots__ = ["first_name"]#!/usr/bin/python3
+    """Define locked class"""
+
+    class LockedClass:
+        ""
+"
+prevents the user from dynamically creatin new instance attributes,
+except if the new instance attribute is called first_name
+"""
+
+__slots__ = ["first_name"]#!/usr/bin/python3
+"""Define locked class"""
+
+class LockedClass:
+    """
+    prevents the user from dynamically creating new instance attributes,
+except if the new instance attribute is called first_name
+"""
+
+__slots__ = ["first_name"]#!/usr/bin/python3
+"""Define locked class"""
+
+class LockedClass:
+    """
+    prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name
+    """
+
+    __slots__ = ["first_name"]#!/usr/bin/python3
+    """Define locked class"""
+
+    class LockedClass:
+        ""
+"
+prevents the user from dynamically creatin new instance attributes,
+except if the new instance attribute is called first_name
+"""
+
+__slots__ = ["first_name"]#!/usr/bin/python3
+"""Define locked class"""
+
+class LockedClass:
+    """
+    prevents the user from dynamically creating new instance attributes,
+except if the new instance attribute is called first_name
+"""
+
+__slots__ = ["first_name"]#!/usr/bin/python3
+"""Define locked class"""
+
+class LockedClass:
+    """
+    prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name
+    """
+
+    __slots__ = ["first_name"]#!/usr/bin/python3
+    """Define locked class"""
+
+    class LockedClass:
+        ""
+"
+prevents the user from dynamically creatin new instance attributes,
+except if the new instance attribute is called first_name
+"""
+
+__slots__ = ["first_name"]#!/usr/bin/python3
+"""Define locked class"""
+
+class LockedClass:
+    """
+    prevents the user from dynamically creating new instance attributes,
+except if the new instance attribute is called first_name
+"""
+
+__slots__ = ["first_name"]
