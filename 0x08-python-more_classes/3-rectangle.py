73534565fd73d51dbc4a07887e793d8478a8797f
Hajar-Hafdi <Hajarlakbir@outlook.com>
2024-10-09 10:12:42 -0700

    fixed errors

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -11,11 +11,11 @@ class Rectangle:
         self.width = width
     @property
     def width(self):
-        """sets private instance attribute width"""
+        """retrieves private instance attribute width"""
         return self.__width
     @width.setter
     def width(self, value):
-        """retriver of the private instance attribute width"""
+        """sets the private instance attribute width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -23,11 +23,11 @@ class Rectangle:
         self.__width = value
     @property
     def height(self):
-        """retriever for private instance attribute height"""
+        """retrieves of private instance attribute height"""
         return self.__height
     @height.setter
     def height(self, value):
-        """retriever for private instance attribute height"""
+        """getter for private instance attribute height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
