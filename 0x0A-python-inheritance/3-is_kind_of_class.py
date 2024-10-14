0a01b532a0a9ad91fd24ca9e9da5b945334e265f
Hajar-Hafdi <Hajarlakbir@outlook.com>
2024-10-14 16:32:46 -0700

    added 3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+'''Mod is_kind_of_class method'''
+
+def is_kind_of_class(obj, a_class):
+    '''Decides if an object is a subclass of a class'''
+    return isinstance(obj, a_class)
