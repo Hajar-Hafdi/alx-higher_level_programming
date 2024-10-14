e2e424208041c553f27ba38868012773069b2ec1
Hajar-Hafdi <Hajarlakbir@outlook.com>
2024-10-14 16:46:14 -0700

    added 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Mod BaseGeometry class'''
+
+class BaseGeometry:
+    '''Rep a BaseGeometry class'''
+    def area(self):
+        '''Technique for calculating this area'''
+        raise Exception('area() is not implemented')
