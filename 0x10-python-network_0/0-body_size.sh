942d8ef141ecfd2e772bbc7bdf3fcc588f0e6014
Hajar Hafdi <hajarlakbir@outlook.com>
2025-01-16 18:34:53 -0800

    fixing errors

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-curl -s "$1" | wc -c
+if [ -z "$1" ]; then
+	  echo "Usage: $0 <URL>"
+	    exit 1
+fi
+size=$(curl -s -o /dev/null -w "%{size_download}" "$1")
+echo "$size"
