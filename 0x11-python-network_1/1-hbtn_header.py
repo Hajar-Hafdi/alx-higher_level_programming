44d47a8a04d958ce506f080b5c416bc95a23842c
Hajar Hafdi <hajarlakbir@outlook.com>
2025-01-17 14:18:17 -0800

    adding files

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-# Python script that takes in a URL, sends a request to the URL and displays
-# the value of the X-Request-Id variable found in the header of the response
+""" Python script that takes in a URL, sends a request to the URL and displays
+ the value of the X-Request-Id variable found in the header of the response
+"""
 import sys
 import urllib.request
 url = sys.argv[1]
 with urllib.request.urlopen(url) as response:
-    # Get the headers from the response
     headers = response.headers
 
-    # Check if 'X-Request-Id' is in the headers
     if 'X-Request-Id' in headers:
         # Print the value of 'X-Request-Id'
         print(headers['X-Request-Id'])
